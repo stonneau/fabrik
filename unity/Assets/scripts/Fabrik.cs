a5792fcdc7b289e5ae801f0a79b032a508cc2ae9
steve tonneau <stevetonneau@hotmail.fr>
2013-08-26 00:47:01 +0200

    ik now working with multiple ef

--- unity/Assets/scripts/Fabrik.cs
+++ unity/Assets/scripts/Fabrik.cs
@@ -1,29 +1,31 @@
 using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 public class Fabrik : MonoBehaviour {
-	
-	public Transform target;
-	public Transform ikChain;
-	
+		
+	/*Constant object maintaining the relation between joints of a skeleton
+	 * iterating through the transforms in a depth-first order.
+	 * */
 	public class JointInfo
 	{
 		public readonly float distanceToParent_; // if 0, no parent
 		public readonly float distanceToRoot_; // if 0, no parent
-		public readonly int id_; // if 0, no parent
+		public readonly int id_; // unique id
 		public readonly JointInfo parent_;
+		public readonly JointInfo fork_; // closest parent with several children
 		public readonly JointInfo[] children_;
-		public readonly JointInfo[] effectors_;
+		public readonly JointInfo[] effectors_; // effectors associated with the chain starting at "this"
 		
-		public JointInfo (Transform joint, ref int id) : this(joint, ref id, null)
+		public JointInfo(Transform joint, ref int id) : this(joint, ref id, null, null)
 		{
 			// NOTHING	
 		}
-		
-		private JointInfo (Transform joint, ref int id, JointInfo parent)
+					
+		private JointInfo(Transform joint, ref int id, JointInfo parent, JointInfo fork)
 		{
 			id_ = id;
+			fork_ = fork;
 			parent_ = parent;
 			if(parent == null)
 			{
@@ -32,13 +34,12 @@ public class Fabrik : MonoBehaviour {
 			}
 			else
 			{
-				distanceToParent_ = Vector3.Distance (joint.parent.position, joint.position);
+				distanceToParent_ = Vector3.Distance(joint.parent.position, joint.position);
 				distanceToRoot_ = parent.distanceToRoot_ + distanceToParent_;
 			}
 			int nbChildren = joint.childCount;
 			children_ = new JointInfo[nbChildren];
-			children_ = new JointInfo[nbChildren];
-			if(joint.childCount == 0) // joint is effector
+			if(nbChildren == 0) // joint is effector
 			{
 				effectors_ = new JointInfo[1];
 				effectors_[0] = this;
@@ -46,10 +47,16 @@ public class Fabrik : MonoBehaviour {
 			else
 			{
 				int nbEffectors_ = 0;
+				JointInfo childFork = fork_;
+				if(nbChildren > 1)
+				{
+					childFork = this;
+				}
 				for(int i=0; i < nbChildren; ++i)
 				{
+					// id is updated with a depth-first iteration
 					++id;
-					JointInfo jInfo = new JointInfo(joint.GetChild(i), ref id, this);
+					JointInfo jInfo = new JointInfo(joint.GetChild(i), ref id, this, childFork);
 					children_[i] = jInfo;
 					nbEffectors_ += jInfo.effectors_.Length;
 				}
@@ -64,113 +71,163 @@ public class Fabrik : MonoBehaviour {
 				}
 			}
 		}
+		public bool Equals(JointInfo jInfo)
+	    {
+	        return jInfo != null && id_ == jInfo.id_;
+	    }
 	}
 	
-	private JointInfo jInfo;
-	private Transform[] transforms;
+	// exposed attributes
+	public Transform ikChain; // kinematic chain on which Ik will be performed
+	public Transform[] targets; // ordered list of targets
+	/*public float treshold;*/ 
+	// End exposed attributes
 	
-	public float treshold;
-		
-	// Use this for initialization
-	void Start ()
+	private JointInfo jointInfo_; // constant
+	private Transform[] transforms_; // transforms associated with our Chain
+	
+	void Start()
 	{
-		int id = 0; // id is updated with a depth-first iteration
-		jInfo = new	JointInfo(ikChain, ref id);
-		transforms = new Transform[id+1];
+		int id = 0;
+		jointInfo_ = new JointInfo(ikChain, ref id);
+		transforms_ = new Transform[id+1];
 		id = 0;
 		InitTransform(ikChain, ref id);
 	}
 	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (target != null)
-		{
-			FabrikStep();
-		}
-	}
-	
+	// indexes transforms of interest
 	private void InitTransform(Transform transform, ref int id)
 	{
-		transforms[id] = transform;
-		for (int i=0; i < transform.GetChildCount(); ++i)
+		transforms_[id] = transform;
+		for(int i=0; i < transform.GetChildCount(); ++i)
 		{
-			++ id;
+			++id;
 			InitTransform(transform.GetChild(i), ref id);
 		}
 	}
 	
-	private void FabrikStep()
+	void Update()
 	{
 		Vector3 rootPos = ikChain.position;
-		// first going from down to up
-		JointInfo effector = jInfo.effectors_[0]; // first approach : one effector only
-		// target too far from tree
-		if(Vector3.Distance(rootPos, target.position)  > effector.distanceToRoot_)
+		Vector3[] positionTargets = new Vector3[targets.Length];
+		for(int i = 0; i < targets.Length; ++i)
 		{
-			IkOutOfRangeTarget(jInfo);
+			positionTargets[i] = targets[i].position;
 		}
-		// target reachable (in theory), make sure that not already reached
-		else if((transforms[effector.id_].position - target.position).magnitude  > treshold)
+		ForwardStep(jointInfo_.effectors_, positionTargets);
+		ikChain.position = rootPos;
+		BackwardStep(jointInfo_);
+	}
+	
+	/*
+	 * Structure allowing to compute the Centroid between different points
+	 * TODO : Target prioritization ?
+	 */
+	private class TargetCentroid
+	{
+		private int nbMatches_;
+		private Vector3 target_;
+		public TargetCentroid()
 		{
-			// forward reaching first
-			transforms[effector.id_].position = target.position;
-			ForwardStep(effector);
-			// then backward reaching 
-			ikChain.position = rootPos;
-			BackwardStep(jInfo);
+			nbMatches_ = 0;
+			target_ = new Vector3(0,0,0);
+		}
+		
+		public void addTarget(Vector3 target)
+		{
+			target_ += target;
+			++nbMatches_;
+		}
+		
+		public Vector3 Target
+		{
+			get {return target_ / Mathf.Max(nbMatches_, 1);}
 		}
 	}
 	
-	private void IkOutOfRangeTarget(JointInfo currentJoint)
+	// First step : from every end effectors go up to closest fork
+	// At fork, determine centroid position for the targets
+	// then go up to the next fork
+	private void ForwardStep(JointInfo[] effectors, Vector3[] targets)
 	{
-		Transform ikChain = transforms[currentJoint.id_];
-		//Find the distance ri between the target t and the joint
-		float r = (target.position - ikChain.position).magnitude;
-		for (int i = 0; i < currentJoint.children_.Length; ++i)
+		Dictionary<JointInfo, TargetCentroid> centroids = new Dictionary<JointInfo, TargetCentroid>();
+		for(int i = 0; i < effectors.Length; ++i)
 		{
-			JointInfo son = currentJoint.children_[i];
-			Transform chainSon = ikChain.GetChild(i);
-			float delta = son.distanceToParent_ / r;
-			Vector3 newPos = (1 - delta) *  ikChain.position + delta * target.position;
-			MoveTransformToPosition(son, chainSon, newPos);
-			IkOutOfRangeTarget(son);
+			JointInfo effector = effectors[i];
+			Vector3 target = targets[i];
+			transforms_[effector.id_].position = target;
+			Vector3 forkTarget = ForwardStepJoint(effector);
+			JointInfo fork = effector.fork_;
+			if(fork != null)
+			{
+				if(!centroids.ContainsKey(fork))
+				{
+					centroids[fork] = new TargetCentroid();
+				}
+				centroids[fork].addTarget(forkTarget);
+			}
+		}
+		int nbCentroids = centroids.Count;
+		if(nbCentroids > 0)
+		{
+			JointInfo[] upEffectors = new JointInfo[nbCentroids];
+			Vector3[] upTargets = new Vector3[nbCentroids];
+			int j = 0;
+			foreach(KeyValuePair<JointInfo, TargetCentroid> pair in centroids)
+			{
+				upEffectors[j] = pair.Key;
+				upTargets[j]   = pair.Value.Target;
+				++j;
+			}
+			ForwardStep(upEffectors, upTargets);
 		}
 	}
 	
-	private void ForwardStep(JointInfo currentJoint)
+	private Vector3 ForwardStepJoint(JointInfo jointInfo)
 	{
-		if(currentJoint.parent_ != null)
+		if(jointInfo.parent_ != null)
 		{
-			Transform transform = transforms[currentJoint.id_];
-			Transform parentTransform = transforms[currentJoint.parent_.id_];
+			Transform transform = transforms_[jointInfo.id_];
+			Transform parentTransform = transforms_[jointInfo.parent_.id_];
 			float r = Vector3.Distance(transform.position, parentTransform.position);
-			float delta = currentJoint.distanceToParent_ / r;
-			Vector3 newPos = (1 - delta) *  transform.position + delta * parentTransform.position;
-			MoveTransformToPosition(currentJoint.parent_, parentTransform, newPos);
-			ForwardStep(currentJoint.parent_);
+			float delta = jointInfo.distanceToParent_ / r;
+			Vector3 newPos =(1 - delta) *  transform.position + delta * parentTransform.position;
+			if(jointInfo.parent_.Equals(jointInfo.fork_)) // parent is fork don't modify position, we'll take centroid
+			{
+				return newPos;
+			}
+			else
+			{
+				MoveTransformToPosition(jointInfo.parent_, newPos);
+				return ForwardStepJoint(jointInfo.parent_);
+			}
 		}
-	}
-	
-	private void BackwardStep(JointInfo currentJoint)
-	{
-		if(currentJoint.children_.Length != 0)
+		else
 		{
-			JointInfo sonJoint = currentJoint.children_[0];
-			Transform transform = transforms[currentJoint.id_];
-			Transform childTransform = transforms[sonJoint.id_];
-			float r = Vector3.Distance(transform.position, childTransform.position);
-			float delta = sonJoint.distanceToParent_ / r;
-			Vector3 newPos = (1 - delta) * transform.position+ delta * childTransform.position;
-			MoveTransformToPosition(sonJoint, childTransform, newPos);
-			BackwardStep(sonJoint);
+			return ikChain.position;
 		}
 	}
 	
-	private void MoveTransformToPosition(JointInfo currentJoint, Transform transform, Vector3 target)
+	private void BackwardStep(JointInfo jointInfo)
 	{
-		if(currentJoint.parent_ != null) transform.position = target;
-		//transform.position = target;
-		//transform.LookAt(target);
+		if(jointInfo.children_.Length != 0)
+		{
+			Transform transform = transforms_[jointInfo.id_];
+			foreach(JointInfo childInfo in jointInfo.children_)
+			{
+				Transform childTransform = transforms_[childInfo.id_];
+				float r = Vector3.Distance(transform.position, childTransform.position);
+				float delta = childInfo.distanceToParent_ / r;
+				Vector3 newPos =(1 - delta) * transform.position+ delta * childTransform.position;
+				MoveTransformToPosition(childInfo, newPos);
+				BackwardStep(childInfo);
+			}
+		}
+	}
+	
+	private void MoveTransformToPosition(JointInfo jointInfo, Vector3 target)
+	{
+		Transform transform = transforms_[jointInfo.id_];
+		if(jointInfo.parent_ != null) transform.position = target;
 	}
 }
